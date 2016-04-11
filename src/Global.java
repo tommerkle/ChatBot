10e1a6f5e48ac0617b3869a38e2e225feafd1a39
tommerkle <tmerk1650@gmail.com>
2016-04-11 12:29:56 -0700

    toString for Message object. Message object done other than q-Types and sentiment

--- src/Global.java
+++ src/Global.java
@@ -6,15 +6,29 @@ import java.util.*;
  */
 public class Global {
 
-    static void printArray(String[] array){
-        for (String s:array
-             ) {
+    static void printArray(String[] array) {
+        for (String s : array
+                ) {
             System.out.println(s);
 
         }
     }
 
-    static void printHashTable(Hashtable<String, String> hashtable){
+    static void printHashTable(Hashtable<String, String> hashtable) {
         System.out.println(hashtable);
+    }
+
+
+    public static String getKey(Hashtable<String, String> pos_table, String value) {
+        //getting a key by its value
+        String key = null;
+        for (Map.Entry entry : pos_table.entrySet()) {
+            if (value.equals(entry.getValue())) {
+                key = (String) entry.getKey();
+
+                break; //breaking because its one to one map
+            }
+        }
+        return key;
     }
 }
--- src/Message.java
+++ src/Message.java
@@ -1,5 +1,7 @@
 
 
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 /**
@@ -9,42 +11,90 @@ public class Message {
 
     public String rawInput; //Store raw input for future use
 
+    //Part of Speech table - <word,POS>
+    public Hashtable<String, String> POS_table;
+
+    //Words by Part in ArrayLists
+    // Limited to nouns, verbs, pronouns, adjectives and adverbas
+    public ArrayList<String> nArray = new ArrayList<String>();
+    public ArrayList<String> pnArray = new ArrayList<String>();
+    public ArrayList<String> vArray = new ArrayList<String>();
+    public ArrayList<String> advArray = new ArrayList<String>();
+    public ArrayList<String> adjArray = new ArrayList<String>();
+
+
     public boolean isQuestion;
-    public String qType; //question type
+    //question type
+    public String qType;
 
 
-    public Hashtable<String, String> POS_table; //Part of Speech table - <word,POS>
-    public Hashtable<String, String> WBP_table; //Words by Part table - <POS,words>.
-                                                // Limited to nouns, verbs, pronouns, adjectives and adverbas
-
-    public int sentiment; //sentiment of the message : positive (1), negative (-1), or neutral (0)
+    //sentiment of the message : positive (1), negative (-1), or neutral (0)
+    public int sentiment;
 
     public Message(String s) {
+        rawInput = s;
         initPOS_table(s);
-        initQtypes(s);
-        POS_table = toHashTable(s);
-
+        initWBP_table();
+        initQtype(s);
+        System.out.println(this.toString());
     }
 
     private void initPOS_table(String s) {
         String tagged = POS_Tagger.tag(s);
-        System.out.println("tagged: " + tagged);
         POS_table = POS_Tagger.makeTable(tagged);
-        System.out.println("after makeTable():");
-        Global.printHashTable(POS_table);
     }
 
-    private void initQtypes(String s) {
+    //We've already parsed the string to create the POS table at this point.
+    //Now we can iterate through the table and save words by their POS
+    //Limit to Nouns,Pronouns, Verbs,  Adverbs, Adjectives
+    private void initWBP_table() {
+       // ArrayList<String> values = new ArrayList<String>(POS_table.values());
+        Enumeration nuValues = POS_table.keys();
+
+        while (nuValues.hasMoreElements()) {
+            String nuVal = (String) nuValues.nextElement();
+            String oldVal = POS_table.get(nuVal);
+            if (oldVal.contains("NN")) {
+                // Noun
+                nArray.add(nuVal);
+            } else if (oldVal.contains("PP$") | oldVal.contains("PRP") | oldVal.contains("WP")) {
+                // Pronoun
+                pnArray.add(nuVal);
+            } else if (oldVal.contains("VB")) {
+                //Verb
+                vArray.add(nuVal);
+            } else if (oldVal.contains("RB")) {
+                //Adverb
+                advArray.add(nuVal);
+            } else if (oldVal.contains("JJ")) {
+                //Adjective
+                adjArray.add(nuVal);
+            }
+        }
+    }
+
+    private void initQtype(String s) {
+        qType = "HAVEN'T FINISHED...";
         /*NOT DONE*/
     }
 
 
-    private Hashtable<String, String> toHashTable(String s) {
-        return null;
-    }
-
     @Override
     public String toString() {
-        return "I am a Message...";
+        String ret = "";
+        ret += "Raw Input: \n" + rawInput;
+        ret += "\n Parts of Speech: \n" + POS_table.toString();
+        ret += "\n Nouns: " + nArray.toString();
+        ret += "\n Pronouns: " + pnArray.toString();
+        ret += "\n Verbs: " + vArray.toString();
+        ret += "\n Adverbs: " + advArray.toString();
+        ret += "\n Adjectives: " + adjArray.toString();
+        ret += "\n Is it a Question?: " + isQuestion;
+        if (isQuestion) {
+            ret += "\n Question Type: " + qType;
+        }
+        ret +="\n Sentiment: " + sentiment;
+
+        return ret;
     }
 }
